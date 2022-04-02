1a65e47332588aa6a7e4c4671033571596255b87
Andrew Midthun <midth002@gmail.com>
2022-04-02 11:35:47 -0500

    Tested an on click event on the search button to target the button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-alert("Weather Dashboard")
+var searchBtn = $('.btn')
+var citySearch = $('.city-search')
+
+
+searchBtn.on("click", function(event) {
+    event.preventDefault();
+    var search = event.target;
+    console.log(search);
+
+    
+})
