95716a0438c57d066fa936400b2c75d2a2387274
Andrew Midthun <midth002@gmail.com>
2022-04-02 17:31:58 -0500

    got the current div to display for when a user inputs a city. Created empty tags in the div container and called them back in the promise function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 var searchBtn = $('.btn')
 var citySearch = $('.city-search')
 var cityForm = $('.city-form')
+var cityName = $('.currentCityName')
+var currentWeather = $('.currentWeather')
+var currentTempEl = $(".current-temp")
+var currentWindEl = $(".current-wind")
+var currentHumidityEl = $('.current-humidity')
+var currentUvEl = $('.current-uv')
+var uvBlock = $('#uv-index')
 var search;
 var apiKey = "385e58697effddc1169cee4d7d6e5489"
 var lat;
@@ -19,7 +26,7 @@ function getCoordinatesAndWeather() {
     
       lat = data[0].lat.toString()
       lon = data[0].lon.toString()
-     console.log(data)
+     
       var url  = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=385e58697effddc1169cee4d7d6e5489&units=imperial"
 
       fetch(url) 
@@ -28,7 +35,26 @@ function getCoordinatesAndWeather() {
     })
     .then(function (data) {
         console.log(data)
-        
+        var temp = data.current.temp.toString()
+        var wind = data.current.wind_speed
+        var humidity = data.current.humidity
+        var uv = data.current.uvi
+        cityName.text(search)
+        currentTempEl.text("Temp: " + temp);
+        currentWindEl.text("Wind: " + wind + " MPH");
+        currentHumidityEl.text("Humidity: " + humidity + "%");
+        currentUvEl.text("UV Index: ");
+        uvBlock.text(uv);
+        uvBlock.attr("style", "background-color: green; padding: 2px; color: white;");
+
+        currentUvEl.append(uvBlock);
+
+        currentWeather.append(cityName);
+        currentWeather.append(currentTempEl);
+        currentWeather.append(currentWindEl);
+        currentWeather.append(currentHumidityEl);
+        currentWeather.append(currentUvEl);
+
     })
 
 
