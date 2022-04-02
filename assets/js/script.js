3b0b5930bc694d6cc994b9db8fde2f9a62ff6e52
Andrew Midthun <midth002@gmail.com>
2022-04-02 16:40:42 -0500

    Changed the API calls to include the geocode to get latitude and longitude and pass that into the onecall api string since the parameters doesn't take just city names.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,42 @@ var searchBtn = $('.btn')
 var citySearch = $('.city-search')
 var cityForm = $('.city-form')
 var search;
+var apiKey = "385e58697effddc1169cee4d7d6e5489"
+var lat;
+var lon;
 
 
+function getCoordinatesAndWeather() {
+    search = citySearch.val();
+    var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + search + "&limit=1&appid=" + apiKey
 
-function getWeatherAPI() {
+    fetch(url) 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
     
-var apiKey ="04365f2ca1de6e59e806c94b06579814"
-var units = "imperial"
-var cityQuery = search
-var url  = "https://api.openweathermap.org/data/2.5/weather?q=" + cityQuery + "&appid=" + apiKey + "&units=" + units
-console.log(url)
+      lat = data[0].lat.toString()
+      lon = data[0].lon.toString()
+     console.log(data)
+      var url  = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=385e58697effddc1169cee4d7d6e5489&units=imperial"
 
-fetch(url) 
+      fetch(url) 
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data)
+        
     })
+
+
+    })
+
+}
+
+
+function renderWeatherAPI() {
 
 }
 
@@ -27,8 +45,9 @@ fetch(url)
 
 searchBtn.on("click", function(event) {
     event.preventDefault();
-    search = citySearch.val();
-    console.log(search);
-    getWeatherAPI();
+    getCoordinatesAndWeather();
+   
 });
+
+
 
