78bd1ce6dff905460595f8be4842b8d13aa1727f
Andrew Midthun <midth002@gmail.com>
2022-04-02 12:02:54 -0500

    Added the function getWeatherAPI() that includes parameters for querying weather api for a city. Included the fetch response to bring back the data to the website in the console log.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
 var searchBtn = $('.btn')
 var citySearch = $('.city-search')
+var cityForm = $('.city-form')
+var search;
+
+
+
+function getWeatherAPI() {
+    
+var apiKey ="04365f2ca1de6e59e806c94b06579814"
+var units = "imperial"
+var cityQuery = search
+var url  = "https://api.openweathermap.org/data/2.5/weather?q=" + cityQuery + "&appid=" + apiKey + "&units=" + units
+console.log(url)
+
+fetch(url) 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+
+}
+
 
 
 searchBtn.on("click", function(event) {
     event.preventDefault();
-    var search = event.target;
+    search = citySearch.val();
     console.log(search);
+    getWeatherAPI();
+});
 
-    
-})
